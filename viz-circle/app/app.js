aab33ee21bf5313fe400fc6106e03916e4df57f6
nicola <me@nicola.io>
2018-04-20 14:12:59 +0900

    adding images for nodes

--- viz-circle/app/app.js
+++ viz-circle/app/app.js
@@ -1,31 +1,44 @@
-const events = {
-  'NewBlockMined': [],
-  'BroadcastBlock': [],
-  'AddAsk': [],
-  'AddBid': [],
-  'MakeDeal': [],
-  'SendFile': [],
-  'BroadcastTxn': [],
-  'SendPayment': []
-  // 'Connected': [],
-  // 'MinerJoins': [],
-  // 'MinerLeaves': [],
-  // 'ClientJoins': [],
-  // 'ClientLeaves': []
-}
+// const events = {
+//   'NewBlockMined',
+//   'BroadcastBlock',
+//   'AddAsk',
+//   'AddBid',
+//   'MakeDeal',
+//   'SendFile',
+//   'BroadcastTxn',
+//   'SendPayment'
+//   // 'Connected',
+//   // 'MinerJoins',
+//   // 'MinerLeaves',
+//   // 'ClientJoins',
+//   // 'ClientLeaves'
+// }
 
-// creates the link
-function send (from, to) {}
-// creates the link with everyone
-function broadcast (icon, from) {}
-// sends something that moves from place to place
-function flight (icon, from, to) {}
-// creates a popup
-function popup (from) {}
-
-// returns random int between 0 and num
-function getRandomInt () {
-  return Math.floor(Math.random() * (num))
+const actions = [
+  {
+    name: 'BroadcastBlock',
+    random (g) {
+      return g.RandomBroadcastMinerToAll()
+    },
+    color: 'gray'
+  },
+  {
+    name: 'SendFile',
+    random (g) {
+      return [g.RandomSend(g.RandomClient(), g.RandomMiner())]
+    },
+    color: 'blue'
+  },
+  {
+    name: 'SendPayment',
+    random (g) {
+      return [g.RandomSend(g.RandomClient(), g.RandomClient())]
+    },
+    color: 'magenta'
+  }
+]
+function getRandomAction () {
+  return actions[Math.floor(Math.random() * actions.length)]
 }
 
 class Graph {
@@ -48,8 +61,8 @@ class Graph {
     return this.clients[Math.floor(Math.random() * this.clients.length)]
   }
 
-  RandomSend () {
-    return {source: graph.RandomMiner(), target: graph.RandomClient()}
+  RandomSend (from, to) {
+    return {source: from, target: to}
   }
 
   RandomBroadcastToMiners () {
@@ -83,8 +96,8 @@ class Graph {
 }
 
 // D3 -- Canvas
-var width = 500
-var height = 500
+var width = 700
+var height = 700
 
 var svg = d3.select('body').append('svg')
     .attr('width', width)
@@ -96,6 +109,8 @@ var circle = svg.append('path')
     .attr('d', 'M 40, ' + (dim / 2 + 40) + ' a ' + dim / 2 + ',' + dim / 2 + ' 0 1,0 ' + dim + ',0 a ' + dim / 2 + ',' + dim / 2 + ' 0 1,0 ' + dim * -1 + ',0')
     .style('fill', '#f5f5f5')
 
+var linesGroup = svg.append('g')
+
 // evenly spaces nodes along arc
 const circleCoord = (circle, node, index, num_nodes) => {
   var circumference = circle.node().getTotalLength()
@@ -104,7 +119,7 @@ const circleCoord = (circle, node, index, num_nodes) => {
   return circle.node().getPointAtLength(circumference - position)
 }
 
-// D3 -- Position nodes around circle
+// D3 -- Drawing the graph
 
 // set up a graph
 const miners = d3.range(10).map(d => { return {id: 'miner' + d} })
@@ -122,57 +137,21 @@ const links = graph.RandomLinks()
 
 // Lines
 
-var lines = svg.selectAll('path.node-link')
-  .data(links).enter()
-  .append('path')
-  .attr('class', 'node-link')
-  .attr('d', d => {
-    var dx = d.target.x - d.source.x,
-      dy = d.target.y - d.source.y,
-      dr = Math.sqrt(dx * dx + dy * dy)
-    return 'M' +
-          d.source.x + ',' +
-          d.source.y + 'A' +
-          dr + ',' + dr + ' 0 0,1 ' +
-          d.target.x + ',' +
-          d.target.y
-  })
-
-const actionLink = graph.RandomBroadcastToMiners()
-var reds = svg.selectAll('path.node-action')
-  .data(actionLink)
-  .enter()
-  .append('path')
-  .attr('class', 'node-action red')
-  .attr('d', d => {
-    var dx = d.target.x - d.source.x,
-      dy = d.target.y - d.source.y,
-      dr = Math.sqrt(dx * dx + dy * dy)
-    return 'M' +
-          d.source.x + ',' +
-          d.source.y + 'A' +
-          dr + ',' + dr + ' 0 0,1 ' +
-          d.target.x + ',' +
-          d.target.y
-  })
- .attr('stroke-dasharray', function () {
-   var totalLength = this.getTotalLength()
-   return totalLength + ' ' + totalLength
- })
-  .attr('stroke-dashoffset', function () {
-    var totalLength = this.getTotalLength()
-    return totalLength
-  })
-  .transition()
-    .duration(600)
-    .ease('linear')
-    .attr('stroke-dashoffset', function () {
-      return 0
-    })
-  .transition()
-    .duration(300)
-    .style('opacity', 0)
-    .remove()
+// var lines = svg.selectAll('path.node-link')
+//   .data(links).enter()
+//   .append('path')
+//   .attr('class', 'node-link')
+//   .attr('d', d => {
+//     var dx = d.target.x - d.source.x,
+//       dy = d.target.y - d.source.y,
+//       dr = Math.sqrt(dx * dx + dy * dy)
+//     return 'M' +
+//           d.source.x + ',' +
+//           d.source.y + 'A' +
+//           dr + ',' + dr + ' 0 0,1 ' +
+//           d.target.x + ',' +
+//           d.target.y
+//   })
 
 // Nodes
 
@@ -186,28 +165,63 @@ var gnodes = svg.selectAll('g.gnode')
     return 'gnode' + ' type-' + d.type
   })
 
-// fades out lines that aren't connected to node d
-const is_connected = (lines, d, opacity) => {
-  lines.transition().style('stroke-opacity', o => {
-    return o.source === d || o.target === d ? 1 : opacity
-  })
-}
-
-var node = gnodes.append('circle')
-    .attr('r', 25)
+var node = gnodes.append('image')
+    .attr('href', d => 'img/' + d.type + '.png')
+    .attr('width', 30)
+    .attr('x', -15)
+    .attr('y', -15)
     .attr('class', (d) => {
       return 'node'
     })
-    .on('mouseenter', function (d) {
-      is_connected(lines, d, 0.1)
-      node.transition().duration(100).attr('r', 25)
-      d3.select(this).transition().duration(100).attr('r', 30)
-    })
-    .on('mouseleave', (d) => {
-      node.transition().duration(100).attr('r', 25)
-      is_connected(lines, d, 1)
-    })
 
 var labels = gnodes.append('text')
-    .attr('dy', 4)
+    .attr('dy', 25)
+    .attr('dx', -15)
     .text(d => { return d.id })
+
+function runAction (g, action) {
+  const actionLinks = action.random(g)
+
+  linesGroup.selectAll('path.node-action')
+    .data(actionLinks)
+    .enter()
+    .append('path')
+    .attr('class', 'node-action action-' + action.name)
+    .attr('d', d => {
+      var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy)
+      return 'M' +
+            d.source.x + ',' +
+            d.source.y + 'A' +
+            dr + ',' + dr + ' 0 0,1 ' +
+            d.target.x + ',' +
+            d.target.y
+    })
+    .attr('stroke', function (d) {
+      return action.color
+    })
+    .attr('stroke-dasharray', function () {
+      var totalLength = this.getTotalLength()
+      return totalLength + ' ' + totalLength
+    })
+    .attr('stroke-dashoffset', function () {
+      var totalLength = this.getTotalLength()
+      return totalLength
+    })
+    .transition()
+      .duration(600)
+      // .ease('linear')
+      .attr('stroke-dashoffset', function () {
+        return 0
+      })
+    .transition()
+      .duration(300)
+      .style('opacity', 0)
+      .remove()
+}
+
+setInterval(() => {
+  const action = getRandomAction()
+  runAction(graph, action)
+}, 300)
