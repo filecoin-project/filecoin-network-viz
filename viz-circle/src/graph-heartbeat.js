0e665deb465f7ed80697fabc891d38431a4b56b4
jbenet <juan@benet.ai>
2018-07-07 00:01:47 -0700

    perf improve heartbeats

--- viz-circle/src/graph-heartbeat.js
+++ viz-circle/src/graph-heartbeat.js
@@ -1,68 +1,126 @@
+const _ = require('underscore')
+
 module.exports = class HeartbeatGraph {
   constructor (target = '#viz-heartbeat .viz') {
+    this.rows = {}
     this.mountPoint = document.querySelector(target)
+    this.DrawSkeleton()
+  }
+
+  DrawSkeleton() {
+    this.mountPoint.innerHTML = `
+      <table class="viz-table">
+        <thead>
+          <tr>
+            <th>PEER</th>
+            <th>CHAIN HEAD</th>
+            <th># PEERS</th>
+            <th># ASKS (latest)</th>
+            <th># BIDS (latest)</th>
+            <th># DEALS</th>
+            <th>WALLETS</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    `
   }
 
   Draw(heartbeats) {
-    const rows = []
-    const header = `
-      <thead>
-        <tr>
-          <th>PEER</th>
-          <th>CHAIN HEAD</th>
-          <th># PEERS</th>
-          <th># ASKS (latest)</th>
-          <th># BIDS (latest)</th>
-          <th># DEALS</th>
-          <th>WALLETS</th>
-        </tr>
-      </thead>
-    `
-
-    const len = a => a ? a.length : 0
-    const msgInfo = info => {
-      if (!info) {
-        return {
-          total: 0,
-          top: 0
-        }
-      }
-
-      const ids = Object.keys(info).map(id => +id)
-
-      return {
-        total: ids.length,
-        top: d3.max(ids, id => id) || 0
-      }
-    }
-
     for (let [peerID, heartbeat] of Object.entries(heartbeats)) {
-      // console.log(heartbeat)
-      const chainhead = heartbeat['best-block']
-      const { total: totalAsks, top: highestAsk } = msgInfo(heartbeat.asks)
-      const { total: totalBids, top: highestBid } = msgInfo(heartbeat.bids)
-      const totalDeals = (heartbeat.deals || []).length
-      const totalPeers = (heartbeat.peers || []).length
-      const wallets = heartbeat['wallet-addrs'].join('<br />')
+      if (!this.rows[peerID]) {
+        this.NewRow(peerID)
+      }
+      this.rows[peerID].Draw(heartbeat)
+    }
+  }
 
-      rows.push(`
-        <tr>
-          <td>${peerID}</td>
-          <td>${chainhead}</td>
-          <td>${totalPeers}</td>
-          <td>${totalAsks} (${highestAsk})</td>
-          <td>${totalBids} (${highestBid})</td>
-          <td>${totalDeals}</td>
-          <td>${wallets}</td>
-        </tr>
-      `)
+  NewRow(peerID) {
+    var tbody = this.mountPoint.querySelector('tbody')
+    var tr = document.createElement('tr')
+    tbody.appendChild(tr)
+    this.rows[peerID] = new HeartbeatRow(peerID, tr)
+  }
+}
+
+class HeartbeatRow {
+  constructor(peerID, el) {
+    this.peerID = peerID
+    this.el = el
+  }
+
+  Draw(nextH) {
+    var next = this.extract(nextH)
+    var last = this._last
+    this._last = next
+
+    if (_.isEqual(next, last))
+      return // no change
+
+    // first time.
+    if (this.el.children.length < 7 || !last) {
+      this.el.innerHTML = `
+        <td>${this.peerID}</td>
+        <td>${next.chainhead}</td>
+        <td>${next.totalPeers}</td>
+        <td>${next.totalAsks} (${next.highestAsk})</td>
+        <td>${next.totalBids} (${next.highestBid})</td>
+        <td>${next.totalDeals}</td>
+        <td>${next.wallets.join('<br />')}</td>
+      `
+      return
     }
 
-    this.mountPoint.innerHTML = `
-      <table class="viz-table">
-        ${header}
-        ${rows.join('\n')}
-      </table>
-    `
+    if (last.chainhead != next.chainhead)
+      this.el.children[1].innerText = next.chainhead
+    if (last.totalPeers != next.totalPeers)
+      this.el.children[2].innerText = next.totalPeers
+    if (last.totalAsks != next.totalAsks)
+      this.el.children[3].innerText = `${next.totalAsks} (${next.highestAsk})`
+    if (last.totalBids != next.totalBids)
+      this.el.children[4].innerText = `${next.totalBids} (${next.highestBid})`
+    if (last.totalDeals != next.totalDeals)
+      this.el.children[5].innerText = next.totalDeals
+    if (_.isEqual(last.wallets, next.wallets))
+      this.el.children[6].innerHTML = next.wallets.join('<br />')
+  }
+
+  extract(h) {
+    if (h.from != this.peerID) {
+      console.log('oh uh, not the right hb:', h.from, this.peerID)
+    }
+
+    var askStats = orderStats(h.asks)
+    var bidStats = orderStats(h.bids)
+    return {
+      chainhead: h['best-block'],
+      totalAsks: askStats.total,
+      highestAsk: askStats.max,
+      totalBids: bidStats.total,
+      highestBid: bidStats.max,
+      totalDeals: (h.deals || []).length,
+      totalPeers: (h.peers || []).length,
+      wallets: h['wallet-addrs']
+    }
+  }
+}
+
+
+function heartbeatsEqual(h1, h2) {
+  // only change when best-block changes.
+  return h1['best-block'] == h2['best-block']
+}
+
+function orderStats(list) {
+  if (!list) {
+    return { total: 0, max: 0 }
+  }
+
+  const ids = Object.keys(list).map(id => +id)
+  ids.push(0) // default
+  return {
+    total: ids.length - 1,
+    max: _.max(ids),
   }
 }
